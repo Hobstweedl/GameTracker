d1ffdd187d2221af33e2984498ed7f4214d56bf8
Jesse Rice <jrice0601@gmail.com>
2015-11-27 11:34:49 -0500

    Players and Winners on playthrough

--- resources/views/playthrough/add.blade.php
+++ resources/views/playthrough/add.blade.php
@@ -39,10 +39,11 @@
     
     <label>Winners</label>
     <div class="ui fluid search multiple selection dropdown" id="winners">
-      <input type="hidden" name="players[]">
+      <input type="hidden" name="winners[]">
       <i class="dropdown icon"></i>
       <div class="default text">Select Winners</div>
-      <div class="menu">
+      <div class="menu list">
+        <!-- List -->
         @foreach($players as $player)
         <div class="item" data-value="{{ $player->id }}">
           {!! $player->profileImage() !!}
@@ -62,7 +63,36 @@
 
 
 <script>
-$('#players').dropdown({placeholder:'Who played?'});
-$('#winners').dropdown({placeholder:'Who won?'});
+$('#players').dropdown({
+  placeholder:'Who played?',
+
+  /*onAdd: function(value, text, $choice) {
+    // custom action
+    console.log(value, text);
+    
+    var ch = $choice[0];
+    var item = '<div class="item" data-value="' + value + '">';
+    item += text;
+    item += '</div>';
+
+    $("#winners .list").append(item);
+    $('#winners').dropdown('refresh');
+
+  },
+  onRemove: function(removedValue, removedText, $removedChoice){
+    var item = $("div").find("[data-value='" + removedValue + "']");
+    $(item).remove();
+    $("#players .list").append(item);
+    $('#winners').dropdown('refresh');
+    $('#players').dropdown('refresh');
+  }
+  */
+});
+
+$('#winners').dropdown({
+  placeholder:'Who won?'
+});
+
+
 </script>
 @endsection
